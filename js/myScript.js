ded57ede63cc65a6fdf1ec4adc45e63cd140fde2
beammieb <beammieb@gmail.com>
2023-05-31 17:05:30 +0100

    set up router, and made current page highlighted as active

---
+++ js/myScript.js
@@ -0,0 +1,77 @@
+//
+//VARIABLE DECLARATIONS
+// 
+
+// [1] - correct
+const global = {
+    currentPage: window.location.pathname
+}
+
+
+
+//
+//FUNCTIONS
+//
+
+//[2] - correct
+//implented a router
+const router = (page) =>{
+
+    switch (page){
+        case "/":
+        case "/index.html":
+            console.log("Home");
+            break;
+        
+        case "/search.html":
+            console.log("Search");
+            break;
+        
+        case "/shows.html":
+            console.log("Shows");
+            break;
+
+        case "/tv-details.html":
+            console.log("TV Details")
+            break;
+        
+        case "/movie-details.html":
+            console.log("Movies Details")
+            break;
+    }
+
+}
+
+//[3] - correct
+//highlight active links
+const highlightActiveLink = (page) =>{
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        if(link.getAttribute('href') === page){
+            link.classList.add('active');
+        }         
+    });
+
+}
+
+
+
+//
+//EVENT HANDLERS
+//
+
+
+
+
+
+
+
+
+
+
+//FUNCTION CALLS ON LOAD
+
+router(global.currentPage);
+
+highlightActiveLink(global.currentPage)
