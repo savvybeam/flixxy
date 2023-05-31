6fa5c4062fd930cfe31b3b1822449fbc77f3bc23
beammieb <beammieb@gmail.com>
2023-05-31 22:54:45 +0100

    commit

--- js/myScript.js
+++ js/myScript.js
@@ -47,7 +47,7 @@ const router = (page) =>{
             break;
 
         case "/tv-details.html":
-            console.log("TV Details")
+            displayShowDetails();
             break;
         
         case "/movie-details.html":
@@ -172,7 +172,7 @@ const displayBackdropImage = (type, imagePath) =>{
     if(type === "movie"){
         document.querySelector('#movie-details').appendChild(overlayDiv);
     }else{
-        document.querySelector('#tv-details').appendChild(overlayDiv);
+        document.querySelector('#show-details').appendChild(overlayDiv);
     }
 
 }
@@ -249,12 +249,93 @@ document.querySelector('#movie-details').appendChild(div);
 }
 
 
+// display single show details
+
+const displayShowDetails = async () =>{
+
+    const showId = window.location.search.split('=')[1];
+
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    console.log(show);
+
+    displayBackdropImage('tv',show.backdrop_path);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+        ${
+        show.poster_path
+            ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+        />`
+            : `<img
+        src="../images/no-image.jpg"
+        class="card-img-top"
+        alt="${show.name}"
+        />`
+        }
+    </div>
+    <div>
+        <h2>${show.name}</h2>
+        <p>
+            <i class="fas fa-star text-primary"></i>
+            ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Air Date: ${show.first_air_date}</p>
+        <p>
+            ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${
+            show.homepage
+        }" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+</div>
+<div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+    <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${show.production_companies
+        .map((company) => `<span>${company.name}</span>`)
+        .join(', ')}
+    </div>
+</div>
+    `;
+
+    document.querySelector('#show-details').appendChild(div);
+}
+
+const showSpinner = () =>{
+    document.querySelector('.spinner').classList.add('show');
+}
+
+const hideSpinner = () =>{
+    document.querySelector('.spinner').classList.remove('show');
+}
+
+
 //get data from TMDB API
 const fetchAPIData = async (endpoint) =>{
+
+    showSpinner();
 
     const response = await fetch(`${global.api.url}${endpoint}?api_key=${global.api.key}&language=en-US`);
     const results = await response.json();
     
+    hideSpinner();
+
     return results;
 
 }
